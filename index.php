b13994e83fa3dc5063539bd1e99a477d098c2b77
brunasant0s-dev <brunaceciliavitorio@gmail.com>
2023-09-25 13:37:32 -0300

    index

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
 
         <a class="listar-button" href="./cliente/consulta_cliente.php">Consultar Clientes</a>
 
-        <a class="listar-button" href="consulta_animal.php">Consultar Animais</a>
+        <a class="listar-button" href="./animal/consulta.animal.php">Consultar Animais</a>
 
-        <a class="listar-button" href="consulta_agenda.php">Consultar Agendamentos</a>
+        <a class="listar-button" href="./agendamento/consulta_agenda.php">Consultar Agendamentos</a>
     </main>
 
 </body>
